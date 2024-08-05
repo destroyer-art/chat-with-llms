3f22d5c0a962939698f7f35ee9d8042735d9c525
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2024-08-06 02:29:48 +0530

    refactor: Add new dependencies for anthropic, transformers, and google-cloud-aiplatform

--- app.py
+++ app.py
@@ -39,6 +39,11 @@ import razorpay
 from razorpay.resources.subscription import Subscription
 from razorpay.resources.customer import Customer
 from razorpay.resources.plan import Plan
+import tiktoken
+from anthropic import Anthropic
+from transformers import AutoTokenizer
+from vertexai.preview import tokenization
+
 
 
 dotenv.load_dotenv()
@@ -87,6 +92,8 @@ customer = Customer(client)
 
 # This will just define that the Authorization header is required
 auth_scheme = HTTPBearer()
+
+anthropic = Anthropic()
 
 class ChatHistory(BaseModel):
     """Chat history model for the request and response."""
@@ -141,133 +148,229 @@ class SubscriptionRequest(BaseModel):
 
 model_company_mapping = {
     "gpt-3.5-turbo": {
-        "model" : ChatOpenAI,
-        "premium" : False,
+        "model": ChatOpenAI,
+        "premium": False,
+        "company": "OpenAI",
+        "input_token_cost_per_million": 0.5,
+        "output_token_cost_per_million": 1.5
     },
     "gpt-4-turbo-preview": {
-        "model" : ChatOpenAI,
-        "premium" : True,
+        "model": ChatOpenAI,
+        "premium": True,
+        "company": "OpenAI",
+        "input_token_cost_per_million": 10.0,
+        "output_token_cost_per_million": 30.0
     },
     "gpt-4": {
-        "model" : ChatOpenAI,
-        "premium" : True,
+        "model": ChatOpenAI,
+        "premium": True,
+        "company": "OpenAI",
+        "input_token_cost_per_million": 30.0,
+        "output_token_cost_per_million": 60.0
     },
-    "gpt-4o-mini" : {
-        "model" : ChatOpenAI,
-        "premium" : False,
+    "gpt-4o-mini": {
+        "model": ChatOpenAI,
+        "premium": False,
+        "company": "OpenAI",
+        "input_token_cost_per_million": 0.15,
+        "output_token_cost_per_million": 0.6
     },
     "gpt-4o": {
-        "model" : ChatOpenAI,
-        "premium" : True,
+        "model": ChatOpenAI,
+        "premium": True,
+        "company": "OpenAI",
+        "input_token_cost_per_million": 5.0,
+        "output_token_cost_per_million": 15.0
     },
-    "claude-3-opus-20240229" : {
-        "model" : ChatAnthropic,
-        "premium" : True,
+    "claude-3-opus-20240229": {
+        "model": ChatAnthropic,
+        "premium": True,
+        "company": "Anthropic",
+        "input_token_cost_per_million": 15.0,
+        "output_token_cost_per_million": 75.0
     },
-    "claude-3-sonnet-20240229" : {
-        "model" : ChatAnthropic,
-        "premium" : True,
+    "claude-3-sonnet-20240229": {
+        "model": ChatAnthropic,
+        "premium": True,
+        "company": "Anthropic",
+        "input_token_cost_per_million": 3.0,
+        "output_token_cost_per_million": 15.0
     },
-    "claude-3-haiku-20240307" : {
-        "model" : ChatAnthropic,
-        "premium" : False,
+    "claude-3-haiku-20240307": {
+        "model": ChatAnthropic,
+        "premium": False,
+        "company": "Anthropic",
+        "input_token_cost_per_million": 0.25,
+        "output_token_cost_per_million": 1.25
     },
-    "claude-3-5-sonnet-20240620" : {
-        "model" : ChatAnthropic,
-        "premium" : True,
+    "claude-3-5-sonnet-20240620": {
+        "model": ChatAnthropic,
+        "premium": True,
+        "company": "Anthropic",
+        "input_token_cost_per_million": 3.0,
+        "output_token_cost_per_million": 15.0
     },
     "mistral-tiny-2312": {
-        "model" : ChatMistralAI,
-        "premium" : False,
+        "model": ChatMistralAI,
+        "premium": False,
+        "company": "Mistral",
+        "input_token_cost_per_million": 0.25,
+        "output_token_cost_per_million": 0.25
     },
     "mistral-small-2312": {
-        "model" : ChatMistralAI,
-        "premium" : False,
+        "model": ChatMistralAI,
+        "premium": False,
+        "company": "Mistral",
+        "input_token_cost_per_million": 0.7,
+        "output_token_cost_per_million": 0.7
     },
     "mistral-small-2402": {
-        "model" : ChatMistralAI,
-        "premium" : False,
+        "model": ChatMistralAI,
+        "premium": False,
+        "company": "Mistral",
+        "input_token_cost_per_million": 1.0,
+        "output_token_cost_per_million": 3.0
     },
     "mistral-medium-2312": {
-        "model" : ChatMistralAI,
-        "premium" : True,
+        "model": ChatMistralAI,
+        "premium": True,
+        "company": "Mistral",
+        "input_token_cost_per_million": 2.7,
+        "output_token_cost_per_million": 8.1
     },
     "mistral-large-2402": {
-        "model" : ChatMistralAI,
-        "premium" : True,
+        "model": ChatMistralAI,
+        "premium": True,
+        "company": "Mistral",
+        "input_token_cost_per_million": 4.0,
+        "output_token_cost_per_million": 12.0
     },
     "gemini-1.0-pro": {
-        "model" : ChatGoogleGenerativeAI,
-        "premium" : False,
+        "model": ChatGoogleGenerativeAI,
+        "premium": False,
+        "company": "Google",
+        "input_token_cost_per_million": 0.5,
+        "output_token_cost_per_million": 1.5
     },
     "gemini-1.5-flash-latest": {
-        "model" : ChatGoogleGenerativeAI,
-        "premium" : False,
+        "model": ChatGoogleGenerativeAI,
+        "premium": False,
+        "company": "Google",
+        "input_token_cost_per_million": 0.35,
+        "output_token_cost_per_million": 1.05
     },
     "gemini-1.5-pro-latest": {
-        "model" : ChatGoogleGenerativeAI,
-        "premium" : True,
+        "model": ChatGoogleGenerativeAI,
+        "premium": True,
+        "company": "Google",
+        "input_token_cost_per_million": 3.5,
+        "output_token_cost_per_million": 10.5
     },
     "sonar-small-chat": {
-        "model" : ChatPerplexity,
-        "premium" : False,
+        "model": ChatPerplexity,
+        "premium": False,
+        "company": "Perplexity",
+        "input_token_cost_per_million": 0.2,
+        "output_token_cost_per_million": 0.2
     },
     "sonar-small-online": {
-        "model" : ChatPerplexity,
-        "premium" : True,
+        "model": ChatPerplexity,
+        "premium": True,
+        "company": "Perplexity",
+        "input_token_cost_per_million": 0.2,
+        "output_token_cost_per_million": 0.2
     },
-    "sonar-medium-chat" : {
-        "model" : ChatPerplexity,
-        "premium" : False,
+    "sonar-medium-chat": {
+        "model": ChatPerplexity,
+        "premium": False,
+        "company": "Perplexity",
+        "input_token_cost_per_million": 0.6,
+        "output_token_cost_per_million": 0.6
     },
-    "sonar-medium-online" : {
-        "model" : ChatPerplexity,
-        "premium" : True,
+    "sonar-medium-online": {
+        "model": ChatPerplexity,
+        "premium": True,
+        "company": "Perplexity",
+        "input_token_cost_per_million": 0.6,
+        "output_token_cost_per_million": 0.6
     },
     "codellama/CodeLlama-34b-Instruct-hf": {
-        "model" : ChatTogether,
-        "premium" : False,
+        "model": ChatTogether,
+        "premium": False,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.78,
+        "output_token_cost_per_million": 0.78
     },
     "codellama/CodeLlama-70b-Instruct-hf": {
-        "model" : ChatTogether,
-        "premium" : True,
+        "model": ChatTogether,
+        "premium": True,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.9,
+        "output_token_cost_per_million": 0.9
     },
     "meta-llama/Llama-2-13b-chat-hf": {
-        "model" : ChatTogether,
-        "premium" : False,
+        "model": ChatTogether,
+        "premium": False,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.22,
+        "output_token_cost_per_million": 0.22
     },
     "meta-llama/Llama-2-70b-chat-hf": {
-        "model" : ChatTogether,
-        "premium" : True,
+        "model": ChatTogether,
+        "premium": True,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.9,
+        "output_token_cost_per_million": 0.9
     },
-    "meta-llama/Llama-3-8b-chat-hf" : {
-        "model" : ChatTogether,
-        "premium" : False,
+    "meta-llama/Llama-3-8b-chat-hf": {
+        "model": ChatTogether,
+        "premium": False,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.2,
+        "output_token_cost_per_million": 0.2
     },
     "meta-llama/Llama-3-70b-chat-hf": {
-        "model" : ChatTogether,
-        "premium" : True,
+        "model": ChatTogether,
+        "premium": True,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.9,
+        "output_token_cost_per_million": 0.9
     },
-    "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo" : {
-        "model" : ChatTogether,
-        "premium" : True,
+    "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo": {
+        "model": ChatTogether,
+        "premium": True,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.7,
+        "output_token_cost_per_million": 0.8
     },
-    "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo" : {
-        "model" : ChatTogether,
-        "premium" : True,
+    "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo": {
+        "model": ChatTogether,
+        "premium": True,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.7,
+        "output_token_cost_per_million": 0.8
     },
-    "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo" : {
-        "model" : ChatTogether,
-        "premium" : True,
+    "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo": {
+        "model": ChatTogether,
+        "premium": True,
+        "company": "Meta",
+        "input_token_cost_per_million": 0.7,
+        "output_token_cost_per_million": 0.8
     },
     "google/gemma-2b-it": {
-        "model" : ChatTogether,
-        "premium" : False,
+        "model": ChatTogether,
+        "premium": False,
+        "company": "Google",
+        "input_token_cost_per_million": 0.1,
+        "output_token_cost_per_million": 0.1
     },
     "google/gemma-7b-it": {
-        "model" : ChatTogether,
-        "premium" : False,
-    },
+        "model": ChatTogether,
+        "premium": False,
+        "company": "Google",
+        "input_token_cost_per_million": 0.2,
+        "output_token_cost_per_million": 0.2
+    }
 }
 
 
@@ -294,7 +397,7 @@ async def add_user_to_db(user_ref, user_data):
         user_ref.set(user_data)
 
 
-def add_message_to_db(request, google_user_id, user_message, ai_message):
+def add_message_to_db(request, google_user_id, user_message, ai_message, stats):
     """
     Background task to add the chat message to the database.
     """
@@ -329,7 +432,8 @@ def add_message_to_db(request, google_user_id, user_message, ai_message):
                     'created_at': google_firestore.SERVER_TIMESTAMP,
                     'updated_at': google_firestore.SERVER_TIMESTAMP,
                     'regenerate_message' : request.regenerate_message,
-                    'model' : request.chat_model
+                    'model' : request.chat_model,
+                    'stats' : stats
                 })
             except Exception as e:
                 logging.error(f'Error updating chat: {e}')
@@ -357,13 +461,47 @@ def add_message_to_db(request, google_user_id, user_message, ai_message):
                 'created_at': google_firestore.SERVER_TIMESTAMP,
                 'updated_at': google_firestore.SERVER_TIMESTAMP,
                 'regenerate_message' : request.regenerate_message,
-                'model' : request.chat_model
+                'model' : request.chat_model,
+                'stats' : stats
             })
         except Exception as e:
             logging.error(f'Error creating new chat: {e}')
             return None
 
     return chat_id
+
+
+def calculate_cost(input_string, output_string, model_name):
+    """
+    Calculate the cost of the chat based on the input and output token lengths.
+    """
+    chat_config = model_company_mapping.get(model_name)
+    input_token_length = 0
+    output_token_length = 0
+
+    if chat_config['company'] == 'OpenAI':
+        encoding = tiktoken.encoding_for_model(model_name)
+        input_token_length = len(encoding.encode(input_string))
+        output_token_length = len(encoding.encode(output_string))
+    
+    if chat_config['company'] == 'Anthropic':
+        input_token_length = anthropic.count_tokens(input_string)
+        output_token_length = anthropic.count_tokens(output_string)
+    
+    if chat_config['company'] == 'Mistral' or chat_config['company'] == 'Perplexity' or chat_config['company'] == 'Meta':
+        encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        input_token_length = len(encoding.encode(input_string))
+        output_token_length = len(encoding.encode(output_string))
+    
+    if chat_config['company'] == 'Google':
+        tokenizer = tokenization.get_tokenizer_for_model("gemini-1.5-flash-001")
+        input_token_length = tokenizer.count_tokens(input_string).total_tokens
+        output_token_length = tokenizer.count_tokens(output_string).total_tokens
+    
+    input_cost = input_token_length * chat_config['input_token_cost_per_million'] / 1000000
+    output_cost = output_token_length * chat_config['output_token_cost_per_million'] / 1000000
+    return input_token_length, output_token_length, input_cost + output_cost
+
 
 @app.exception_handler(Exception)
 async def generic_exception_handler(request, exc):
@@ -564,6 +702,9 @@ async def chat_event_streaming(request: ChatRequest, token_info: dict = Depends(
         
         generated_ai_message = ""
 
+        # convert into a total input string
+        total_input = prompt.format(chat_history=memory.buffer, user_input=request.user_input)
+        
         # Run the conversation.invoke method in a separate thread
         def event_streaming():
             nonlocal generated_ai_message
@@ -573,9 +714,20 @@ async def chat_event_streaming(request: ChatRequest, token_info: dict = Depends(
                     response = ChatEventStreaming(event="stream", data=token, is_final=False)
                     yield f"data: {json.dumps(jsonable_encoder(response))}\n\n"
                 
+                
+                input_token_length, output_token_length, cost = calculate_cost(total_input, generated_ai_message, chat_model)
+
+                # stats for the chat
+                stats = {
+                    "input_token_length": input_token_length,
+                    "output_token_length": output_token_length,
+                    "cost": cost
+                }
+
+                print("Stats: ", stats)
 
                 # Database update after streaming is completed
-                chat_id = add_message_to_db(request, token_info['sub'], request.user_input, generated_ai_message)
+                chat_id = add_message_to_db(request, token_info['sub'], request.user_input, generated_ai_message, stats)
 
                 response = ChatEventStreaming(event="stream", data="", is_final=True, chat_id=chat_id)
                 yield f"data: {json.dumps(jsonable_encoder(response))}\n\n"
